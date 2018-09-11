a5004ad5c251d8bf08053dab6e23787a297ab6ba
unknown <matthew.teja@gmail.com>
2018-09-11 16:08:49 -0700

    Initial commit

---
+++ app/src/main/java/com/gerrymatthewnick/randomsideproject/bullethellgame/MainActivity.java
@@ -0,0 +1,27 @@
+package com.gerrymatthewnick.randomsideproject.bullethellgame;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void mainToOnePlayer(View view) {
+        Intent intent = new Intent(this, SinglePlayerChoose.class);
+        startActivity(intent);
+    }
+
+    public void mainToTwoPlayers(View view) {
+        Intent intent = new Intent(this, TwoPlayerGame.class);
+        startActivity(intent);
+    }
+
+
+}
---
+++ app/src/main/java/com/gerrymatthewnick/randomsideproject/bullethellgame/TwoPlayerGame.java
@@ -0,0 +1,13 @@
+package com.gerrymatthewnick.randomsideproject.bullethellgame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TwoPlayerGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two_player_game);
+    }
+}
