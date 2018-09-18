878b87315e41bc3958caac42d78c0c9e44f88490
Matthew Teja <matthew.teja@gmail.com>
2018-09-17 21:37:18 -0700

    Made bullet move

--- app/src/main/java/com/gerrymatthewnick/randomsideproject/bullethellgame/Bullet1.java
+++ app/src/main/java/com/gerrymatthewnick/randomsideproject/bullethellgame/Bullet1.java
@@ -1,5 +1,6 @@
 package com.gerrymatthewnick.randomsideproject.bullethellgame;
 
+import android.app.Activity;
 import android.content.Context;
 import android.widget.ImageView;
 
@@ -15,10 +16,18 @@ public class Bullet1 extends Projectile {
 
     public void init() {
 
+        //in the future, set this to support all rls
+        rl = ((Activity)con).findViewById(R.id.rlSingleCursor);
+
         super.image = new ImageView(con);
         super.image.setImageResource(con.getResources().getIdentifier("bullet1", "drawable", con.getPackageName()));
-        super.velocity = bulletVelocity;
 
+        setImage();
+        super.velocity = bulletVelocity;
+        startMove();
 
     }
 }
+//TODO delete image after leaves screen
+//TODO detect if activity is active
+//TODO check if image touches cursor
--- app/src/main/java/com/gerrymatthewnick/randomsideproject/bullethellgame/Projectile.java
+++ app/src/main/java/com/gerrymatthewnick/randomsideproject/bullethellgame/Projectile.java
@@ -3,6 +3,7 @@ package com.gerrymatthewnick.randomsideproject.bullethellgame;
 
 import android.os.Handler;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 public class Projectile {
 
@@ -12,17 +13,29 @@ public class Projectile {
     public ImageView image;
     public Cursor cursor;
 
+    public RelativeLayout rl;
+
+    public void setImage() {
+        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        image.setLayoutParams(lp);
+        image.setX(200);
+        image.setY(200);
+        rl.addView(image);
+
+    }
 
     Runnable moveRunnable = new Runnable() {
         @Override
         public void run() {
-
+            check();
+            image.setY(image.getY() + velocity);
 
             moveImage.postDelayed(moveRunnable, 10);
         }
     };
     //moves projectile by velocity
     public void startMove() {
+        moveRunnable.run();
 
     }
 
