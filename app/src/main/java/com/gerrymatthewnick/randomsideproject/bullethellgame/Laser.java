e96dbeb972885edfbee7592fd5dbfeb9c11e238e
Matthew Teja <matthew.teja@gmail.com>
2018-09-20 01:29:20 -0700

    Added laser width and height adjustment

--- app/src/main/java/com/gerrymatthewnick/randomsideproject/bullethellgame/Laser.java
+++ app/src/main/java/com/gerrymatthewnick/randomsideproject/bullethellgame/Laser.java
@@ -1,6 +1,7 @@
 package com.gerrymatthewnick.randomsideproject.bullethellgame;
 
 import android.content.Context;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
@@ -21,8 +22,17 @@ public class Laser {
     public void setImage() {
         RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
         image.setLayoutParams(lp);
+
         image.setX(xLeft);
         image.setY(yTop);
+
+        image.setAdjustViewBounds(true);
+        image.setScaleType(ImageView.ScaleType.CENTER_CROP);
+
+        image.getLayoutParams().height = yHeight;
+        image.getLayoutParams().width = xWidth;
+
+        //might need to draw rectangle
         rl.addView(image);
 
     }
